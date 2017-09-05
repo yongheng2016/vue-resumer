aa176d167020f513bab8ac8c787264031678ca76
yongheng <1074827582@qq.com>
2017-09-05 11:42:31 +0800

    add scss

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,6 +2,10 @@
   <div id="Topbar">
       <div class="wrapper">
           <span class="logo">Resumer</span>
+          <div class="actions">
+              <button class="primary">保存</button>
+              <button>预览</button>
+          </div>
       </div>
   </div>
 </template>
@@ -12,20 +16,42 @@ export default {
 }
 </script>
 
-<style>
-    #Topbar>.wrapper {
-        min-width: 1024px;
-        max-width: 1440px;
-        margin: 0 auto;
-        height: 64px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+<style scoped lang="scss">
+    #Topbar {
+        background: #fff;
+        box-shadow: 0px 1px 3px 0 rgba(0,0,0,.25);
+        >.wrapper {
+            min-width: 1024px;
+            max-width: 1440px;
+            margin: 0 auto;
+            height: 64px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 16px;
+        }
+        .logo {
+            font-size: 24px;
+            color: #000;
+        }
     }
-    #Topbar .logo {
-        font-size: 24px;
-        color: #000;
+    button {
+        width: 72px;
+        height: 32px;
+        border: none;
+        cursor: pointer;
+        font-size: 18px;
+        background: #ddd;
+        color: #222;
+        &:hover {
+            box-shadow: 1px 1px 1px hsla(0,0,0,.5);
+        }
+        &.primary {
+            background: #02af5f;
+            color: #fff;
+        }
     }
+    
 </style>
 
 
