b020bf45bfff496462b809a64177eca0332b53b4
yongheng <1074827582@qq.com>
2017-09-05 12:31:04 +0800

    添加Editor的导航栏和内容区

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,19 +1,67 @@
 <template>
   <div id="Editor">
-      I am Editor
+      <nav>
+          <ol>
+              <li class="active">x</li>
+              <li>x</li>
+              <li>x</li>
+              <li>x</li>
+              <li>x</li>
+              <li>x</li>
+              <li>x</li>
+          </ol>
+      </nav>
+      <ol class="panels">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+      </ol>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Editor'
+  name: 'Editor',
+  data(){
+      return {
+
+      }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
     #Editor {
         background: #ffffff;
         box-shadow: 0px 1px 3px 0 rgba(0,0,0,.25);
+        display: flex;
+        flex-direction: row;
+        > nav {
+            width: 80px;
+            background: #000;
+            color: #fff;
+            > ol {
+                > li {
+                    height: 48px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    margin-top: 16px;
+                    margin-bottom: 16px;
+                    &.active {
+                        background: #fff;
+                        color: #000;
+                    }
+                }
+            }
+        }
+        ol {
+            list-style: none;
+        }
     }
 </style>
 
